afb6e6b85d46f3f9561ddea7a609262d1b988ca1
Kunal Agarwal <kunalagarwal9204@gmail.com>
2024-06-05 19:04:36 +0530

    dataBase connection established,user model deign and folder name of each backend is created

---
+++ Backend/db/dbConnect.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+module.exports = connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB", error.message);
+  }
+};
